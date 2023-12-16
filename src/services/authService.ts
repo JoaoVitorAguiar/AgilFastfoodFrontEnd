60bcd70c67deb4275c51f162e79cd35976d7b6e5
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2023-12-16 11:30:36 -0300

    fix: resolve redirect problem

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -16,12 +16,36 @@ interface RegisterData {
   password_confirmation: string;
 }
 
+interface UserData {
+  fullName: string;
+  // Adicione outros campos do usuário conforme necessário
+}
+
 export const login = async (data: LoginData) => {
-  const response = await axios.post(`${API_URL}/sessions/create`, data);
-  if (response.data.token) {
-    localStorage.setItem('token', response.data.token);
+  try {
+    const response = await http.post(`/sessions`, data);
+    const { token, user } = response.data;
+
+    // Salve o token JWT no armazenamento local
+    localStorage.setItem('token', token);
+
+    // Salve as informações do usuário no armazenamento local
+    localStorage.setItem('user', JSON.stringify(user));
+    // Recarregar a página após o login
+
+    return response.data;
+  } catch (error: any) {
+    // Agora, estamos usando a sintaxe de TypeScript para especificar que 'error' é do tipo 'any'
+    throw error.response ? error.response.data : 'Erro desconhecido';
   }
-  return response.data;
+};
+
+export const logout = () => {
+  // Limpe as informações de autenticação e do usuário (por exemplo, remova o token e o usuário do armazenamento local)
+  localStorage.removeItem('token');
+  localStorage.removeItem('user');
+  // Recarregar a página após o logout
+  window.location.reload(); 
 };
 
 export const register = async (data: RegisterData) => {
@@ -45,8 +69,34 @@ export const register = async (data: RegisterData) => {
     }
 };
   
-  
+
+export const getUser = (): string | null => {
+  const userString = localStorage.getItem('user');
+  // alert(userString)
+  // Verifique se o userString não é null ou undefined antes de chamar JSON.parse
+  if (userString) {
+    try {
+      return JSON.parse(userString);
+    } catch (error) {
+      console.error('Erro ao analisar JSON de usuário:', error);
+      return null;
+    }
+  }
+
+  return null;
+};
+
+export const isAuthenticated = (): boolean => {
+  // Verifique se há um token no armazenamento local
+  const token = localStorage.getItem('token');
+  return !!token;
+};
 
 export const getToken = () => {
   return localStorage.getItem('token');
+};
+
+export const logoutBeforeRegister = async () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('user');
 };
