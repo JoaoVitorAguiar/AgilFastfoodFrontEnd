038495583c134337d95442bf205fc6a799e4e9d0
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2023-12-16 11:29:59 -0300

    feat: add component NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,46 @@
 // src/components/NavBar.tsx
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState} from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { logout, getUser, isAuthenticated } from '../services/authService';
+import logo from '../img/logo.png'
+import './NavBar.css'
+
 
 const NavBar: React.FC = () => {
+  const history = useHistory();
+  const [user, setUser] = useState(getUser());
+  const [userData, setUserData] = useState(null);
+  const handleLogout = () => {
+    // Chame a função de logout do authService
+    logout();
+    // Redirecione para a página de login ou outra página desejada após o logout
+    history.push('/login');
+  };
+
   return (
-    <nav>
-      <ul style={{ listStyleType: 'none', display: 'flex', justifyContent: 'space-around' }}>
-        <li>
-          <Link to="/">Home</Link>
+    <nav className='navbar'>
+      <ul>
+        <li className='logo'>
+        <Link to="/">
+            <img src={logo} alt="Logo" />
+          </Link>
         </li>
         <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Cadastro</Link>
+          {/* Exibe "Entrar" se não estiver autenticado, ou "Sair" se estiver autenticado */}
+          {isAuthenticated() ? (
+            <>
+              
+                {/* Exiba o nome do usuário */}
+                <p>Olá, {user}!</p>
+              
+                <button onClick={handleLogout}>Sair</button>
+              
+            </>
+          ) : (
+            <>
+              <Link to="/login"> <button>Entrar</button></Link>
+            </>
+          )}
         </li>
       </ul>
     </nav>
