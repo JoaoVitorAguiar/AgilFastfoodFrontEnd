5b35a74f97f368e9897847d39eb086374351ce39
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2023-12-16 11:29:01 -0300

    feat: add home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,39 @@
 // src/pages/HomePage.tsx
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import MenuItem from '../components/MenuItem';
+import '../styles/HomePage.css'
+import axios from 'axios';
+
+interface Food {
+  name: string;
+  description: string;
+  price: number;
+}
 
 const HomePage: React.FC = () => {
+  const [menuItems, setMenuItems] = useState<Food[]>([]);
+
+  useEffect(() => {
+    // Faz a requisição à API quando o componente é montado
+    axios.get('http://localhost:3333/foods/list')
+      .then(response => {
+        setMenuItems(response.data);
+      })
+      .catch(error => {
+        console.error('Erro ao obter lista de alimentos:', error);
+      });
+  }, []); // O array vazio como segundo argumento garante que o efeito é executado apenas uma vez, equivalente ao componentDidMount
+
   return (
-    <div>
-      <h1>Página Inicial</h1>
-      {/* Conteúdo da página inicial */}
+    <div className="container">
+      <h2>Welcome to our Fast Food Catalog</h2>
+      <div className="menu">
+        {menuItems.map((item, index) => (
+          <div key={index} className="menu-item">
+            <MenuItem {...item} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
