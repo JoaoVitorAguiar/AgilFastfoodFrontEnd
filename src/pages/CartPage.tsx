ae4aa7c0c765da2c011dfb4d2fd2b16a3149714d
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2023-12-18 17:22:37 -0300

    feat: add order request

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,17 +1,65 @@
 // src/pages/CartPage.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import { useCart } from '../contexts/CartContext';
-import '../styles/CartPage.css'
-import { isAuthenticated } from '../services/authService';
-import { Link } from 'react-router-dom';
+import '../styles/CartPage.css';
+import { isAuthenticated, getToken } from '../services/authService';
+import http from '../services/httpService';  // Importe o serviço HTTP
+import { Link, Redirect } from 'react-router-dom';
 
 const CartPage: React.FC = () => {
   const { cartItems, removeFromCart, clearCart, incrementQuantity, decrementQuantity } = useCart();
-  const handleCheckout = () => {
-    // Lógica para finalizar o pedido (pode ser redirecionamento para uma página de checkout)
-    console.log('Pedido finalizado!');
+  const [checkoutSuccess, setCheckoutSuccess] = useState(false); // Novo estado para controlar a exibição da mensagem
+
+  const handleCheckout = async () => {
+    try {
+      const token = getToken();
+
+      if (!token) {
+        // Trate o caso em que o token não está presente (usuário não autenticado)
+        console.error('Usuário não autenticado');
+        return;
+      }
+
+      // Construa a lista de itens para o pedido
+      const orderItems = cartItems.map(item => ({
+        foodId: item.id,
+        quantity: item.quantity,
+      }));
+
+      // Crie a solicitação para finalizar o pedido usando o serviço HTTP
+      const response = await http.post('/orders', {
+        foods: orderItems,
+        // Outros campos do pedido, se necessário
+      });
+
+      if (response.status === 200) {
+        console.log('Pedido finalizado com sucesso!');
+        // Atualize o estado para exibir a mensagem
+        clearCart();
+        setCheckoutSuccess(true);
+        // Pode adicionar lógica adicional aqui, se necessário
+      } else {
+        console.error('Erro ao finalizar o pedido:', response.statusText);
+        // Trate o erro de acordo (pode ser exibindo uma mensagem para o usuário, etc.)
+      }
+    } catch (error) {
+      console.error('Erro ao finalizar o pedido:', error);
+    }
   };
+
   const totalPrice = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+
+  if (checkoutSuccess) {
+    // Exibe a mensagem após o checkout e redireciona para a página inicial
+    return (
+      <div className="CheckoutSuccessContainer">
+        <p>Pedido finalizado com sucesso!</p>
+        <Link to="/">
+          <button className="OKButton">OK</button>
+        </Link>
+      </div>
+    );
+  }
 
   return (
     <div className="CartContainer">
