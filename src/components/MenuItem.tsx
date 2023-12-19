81bef7b14a8b13f38fa1199260c9e62750d362c3
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2023-12-19 20:13:27 -0300

    fix: refactor the layout

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { useCart, FoodItem } from '../contexts/CartContext';
+import Price from './Price'; // Certifique-se de que o caminho esteja correto
 
 interface MenuItemProps {
   item: FoodItem; 
 }
-const MenuItem: React.FC<MenuItemProps> = ({ item}) => {
+
+const MenuItem: React.FC<MenuItemProps> = ({ item }) => {
   const { addToCart } = useCart(); 
+
   return (
     <div className='menu-item-content'>
       <h3>{item.name}</h3>
       <p>{item.description}</p>
-      <p>Price: ${item.price.toFixed(2)}</p>
-      <button onClick={() => addToCart(item)}>Adicionar ao Carrinho</button>
+      <Price value={item.price} />
+      <button className='addToCard' onClick={() => addToCart(item)}>Adicionar ao Carrinho</button>
     </div>
   );
 };
